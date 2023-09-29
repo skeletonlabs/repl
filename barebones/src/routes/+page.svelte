6716c40b693450939ea14da13347973028f6d08c
endigo9740 <gundamx9740@gmail.com>
2023-09-29 12:05:23 -0500

    Updated barebones to Skeleton v2

--- barebones/src/routes/+page.svelte
+++ barebones/src/routes/+page.svelte
@@ -5,9 +5,9 @@
 		<h1 class="h1">Let's get cracking bones!</h1>
 		<p>Start by exploring:</p>
 		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
+			<li><code class="code">/src/routes/+layout.svelte</code> - the root layout in your project.</li>
+			<li><code class="code">/src/routes/+page.svelte</code> - the homepage contents.</li>
+			<li><code class="code">/src/app.postcss</code> - your global stylesheet.</li>
 		</ul>
 	</div>
 </div>
